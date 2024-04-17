0335edde79178074bb35d4856103a889cb20f2b0
Radhitka <raditfatner47@gmail.com>
2024-04-17 18:23:05 +0700

    feat: First Commit

---
+++ index.js
@@ -0,0 +1,54 @@
+import 'dotenv/config';
+import express from 'express';
+import http from 'http';
+import mustacheExpress from 'mustache-express';
+import { dirname } from 'path';
+import { Server } from 'socket.io';
+import { fileURLToPath } from 'url';
+// const express = require('express');
+// const mustacheExpress = require('mustache-express');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+const port = process.env.PORT || 3000;
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', mustacheExpress());
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+io.on('connection', (socket) => {
+  console.log('User terhubung..');
+
+  socket.on('user-login', (data) => {
+    console.log(`${data} Loginn`);
+    socket.username = data;
+    socket.broadcast.emit('user-login', `${data} telah bergabung!`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User menghilang..');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', `${socket.username} : ${msg}`);
+  });
+
+  socket.on('typing', (msg) => {
+    socket.broadcast.emit('typing', msg);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
